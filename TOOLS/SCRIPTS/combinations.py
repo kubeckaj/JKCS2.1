aca1c3a80cbc5b8defdc3cb644792e71b26e9f03
Kubecka <akcebukbukaj@gmail.com>
2020-11-11 23:30:24 +0100

    combinations in JKCS1

--- TOOLS/SCRIPTS/combinations.py
+++ TOOLS/SCRIPTS/combinations.py
@@ -1,5 +1,6 @@
 import numpy
 import sys
+#import math
 
 #numpy: asarray, array
 def product(*args, **kwds):
@@ -12,6 +13,15 @@ def product(*args, **kwds):
     for prod in result:
         yield tuple(prod)
 
+def productJK(*args, **kwds):
+    # product('ABCD', 'xy') --> Ax Ay Bx By Cx Cy Dx Dy
+    # product(range(2), repeat=3) --> 000 001 010 011 100 101 110 111
+    pools = map(tuple, args) * kwds.get('repeat', 1)
+    result = [[]]
+    for pool in pools:
+        result = [x+[y] for x in result for y in pool if sum(x)+y<=max(max(args))]
+    for prod in result:
+        yield tuple(prod)
 
 #sys.argv(1)
 maximum=map(int,sys.argv[2].split(',')[1:])
@@ -21,9 +31,11 @@ totcharge=int(sys.argv[4])
 npcharges=numpy.array(charges)
 
 #maximum=map(int,maximum)
+#print("maximum")
 #print(maximum)
 #print(structures)
 #print(type(structures))
+#print("len(maximum)")
 #print(len(maximum))
 #print(maximum[0])
 #print(size(npcharges))
@@ -32,10 +44,16 @@ list2=[]
 listch=[]
 ch1=0
 for j in range(len(maximum)):
-  comb = product(range(maximum[j]+1),repeat=structures[j])
+  #print("LOOP"+str(j))
+  comb = productJK(range(maximum[j]+1),repeat=structures[j])
   list1=numpy.empty([0,structures[j]], int)
-  #print(numpy.size(list1,1))
+  #print("NUMPY SIZE "+str(numpy.size(list1,1)))
+  #print("comb:")
+  #print(comb)
+  #print("list comb:")
+  #print(list(comb))
   for i in list(comb): 
+    #print("LOOP"+str(j)+str(i))
     #print(numpy.size(numpy.asarray(i,int)))
     toappend=numpy.asarray(i,int)
     #print(toappend)
@@ -49,7 +67,32 @@ for j in range(len(maximum)):
   ch1=ch2
   #del list1
 
+#for j in range(len(maximum)):
+#  print("LOOP"+str(j))
+#  comb = product(range(maximum[j]+1),repeat=structures[j])
+#  list1=numpy.empty([0,structures[j]], int)
+#  print("NUMPY SIZE "+str(numpy.size(list1,1)))
+#  #print("comb:")
+#  #print(comb)
+#  print("list comb:")
+#  print(list(comb))
+#  for i in list(comb): 
+#    print("LOOP"+str(j)+str(i))
+#    print(numpy.size(numpy.asarray(i,int)))
+#    toappend=numpy.asarray(i,int)
+#    print(toappend)
+#    print(numpy.sum(toappend))
+#    if numpy.sum(toappend)==maximum[j]:
+#      list1=numpy.concatenate((list1,[toappend]),axis=0)
+#  print(list1)
+#  list2.append(list1)
+#  ch2=ch1+structures[j]
+#  listch.append(charges[ch1:ch2])
+#  ch1=ch2
+#  #del list1
+
 #print(list2)
+#print("DONE")
 listJK=[]
 for i in product(*list2): 
   toappend=numpy.concatenate(i)
