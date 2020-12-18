eb3c9611593933d438f9b7e27933d49a29ff0398
Kubecka <akcebukbukaj@gmail.com>
2020-12-18 11:22:39 +0100

    test

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+CURRDIR=$PWD
 echo ___________________
 echo TESTING JKCS SETUP:
 
@@ -26,7 +27,9 @@ else
   fi
 fi
 ##########
+cd $WRKDIR
 printf "== testing Python2.x:"
+printf "== testing Python2.x:\n" >> .log
 if [ -e .help ]; then rm .help; fi
 program_PYTHON2 --version > .help 2>&1
 result=`cat .help | tail -n 1 | cut -c-9`
@@ -37,6 +40,7 @@ then
   echo "a=numpy.matrix('1 2;3 4')" >> .test.py
   echo "print(a*a)" >> .test.py
   program_PYTHON2 .test.py > .test.out 2> .test.out
+  cd $WRKDIR
   result=`grep -c "[15 22]]" .test.out`
   if [ $result -eq 1 ]
   then
@@ -57,7 +61,9 @@ fi
 rm .help
 ##########
 ##########
+cd $WRKDIR
 printf "== testing Python3.x:"
+printf "== testing Python3.x:\n" >> .log
 if [ -e .help ]; then rm .help; fi
 program_PYTHON3 --version > .help 2>&1
 result=`cat .help| tail -n 1 |cut -c-9`
@@ -68,6 +74,7 @@ then
   #echo "a=numpy.matrix('1 2;3 4')" >> .test.py
   echo "print(a)" >> .test.py
   program_PYTHON3 .test.py > .test.out 2> .test.out
+  cd $WRKDIR
   result=`grep -c "30" .test.out`
   if [ $result -eq 1 ]
   then
@@ -87,9 +94,12 @@ else
 fi
 rm .help
 ##########
+cd $WRKDIR
 printf "== testing ABCluster:"
+printf "== testing ABCluster:\n" >> .log
 touch .calc.inp
 program_ABC .calc.inp 2> .calc.out
+cd $WRKDIR
 result=`grep -c "Cannot read the cluster file name." .calc.out`
 if [ $result -eq 1 ]
 then 
@@ -103,13 +113,16 @@ else
 fi
 rm .calc.inp .calc.out
 ##########
+cd $WRKDIR
 printf "== testing XTB:"
+printf "== testing XTB:\n" >> .log
 if [ -e .test.xyz ]
 then 
   rm .test.xyz
 fi
 touch .test.xyz
 program_XTB .test.xyz > .test.log 2> .test.log
+cd $WRKDIR
 result=`grep -c "#ERROR! no atoms!" .test.log`
 if [ $result -eq 1 ]
 then
@@ -123,9 +136,12 @@ else
 fi
 rm .test.xyz .test.log
 ##########
+cd $WRKDIR
 printf "== testing Gaussian(G16):"
+printf "== testing Gaussian(G16):\n" >> .log
 touch .test.com
 program_G16 .test.com > .test.log 2> .test.log
+cd $WRKDIR
 result=`grep -c "Route card not found." .test.log`
 if [ $result -eq 1 ]
 then
@@ -139,9 +155,12 @@ else
 fi
 rm .test.log .test.com
 ##########
+cd $WRKDIR
 printf "== testing GoodVibes:"
+printf "== testing GoodVibes:\n" >> .log
 touch .test.log
 program_GoodVibes .test.log > .test.out 2> /dev/null 
+cd $WRKDIR
 result=`grep -c "Warning! Couldn't " .test.out`
 if [ $result -eq 1 ]
 then
@@ -156,9 +175,13 @@ fi
 rm .test.out .test.log
 if [ -e Goodvibes_output.dat ]; then rm Goodvibes_output.dat; fi
 ##########
+cd $WRKDIR
 printf "== testing ORCA:"
-touch .test.inp
-program_ORCA .test.inp > .test.out 2> .test.out
+printf "== testing ORCA:\n" >> .log
+touch .test.inp 
+program_ORCA .test.inp > .test.out  2> .test.out
+cd $WRKDIR
+if [ ! -e .test.out ]; then touch .test.out; fi
 result=`grep -c "You must have a \[COORDS\] ... \[END\] block in your input" .test.out`
 if [ $result -ne 0 ]
 then
@@ -170,8 +193,9 @@ else
   echo "   :: see .log for the error"
   echo "   :: open ~/.JKCSusersetup -> check program_ORCA or setup properly path PATH_ORCA"
 fi
-rm .test.inp .test.out .test.xyz  2>/dev/null
+cd $CURRDIR
 echo ___________________
+rm .test.inp .test.inp.out .test.out .test.xyz  2>/dev/null
 rm .*.cmd 2>/dev/null
 rm .*.out 2>/dev/null
 rm .*.log 2>/dev/null
