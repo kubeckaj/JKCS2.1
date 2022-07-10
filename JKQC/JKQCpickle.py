43bd62749b468496ee85d86075facd1f7c9f8d2f
Jakub Kubecka <kubeckaj@puhti-login2.bullx>
2022-07-10 23:07:37 +0300

    corr

--- JKQC/JKQCpickle.py
+++ JKQC/JKQCpickle.py
@@ -1254,9 +1254,11 @@ if Qqha == 1:
   if Qanh != 1:
     # VIBRATIONAL FREQ MODIFICATION e.g. anharmonicity (vib.freq.,ZPE,ZPEc,U,Uc,H,Hc,S // G,Gc)
     for i in range(len(clusters_df)):
-      if pd.isna(clusters_df["log"]["vibrational_frequencies"].values[i]).any():
-        continue
-      QtOLD = clusters_df["log","temperature"].values[i]
+      try:
+        if pd.isna(clusters_df["log"]["vibrational_frequencies"].values[i]).any():
+          continue
+      except:
+        QtOLD = clusters_df["log","temperature"].values[i]
       try:
         lf = float(clusters_df["log"]["vibrational_frequencies"].values[i][0])
       except:
@@ -1291,10 +1293,12 @@ if Qqha == 1:
   # NEW TEMPERATURE (T,S,H,Hc,U,Uc // G,Gc)
   if ~np.isnan(Qt):
     for i in range(len(clusters_df)):
-      if pd.isna(clusters_df["log"]["vibrational_frequencies"].values[i]).any():
-        clusters_df["log","temperature"][i] = Qt
-        continue
-      QtOLD = clusters_df["log","temperature"].values[i]
+      try:
+        if pd.isna(clusters_df["log"]["vibrational_frequencies"].values[i]):
+          clusters_df["log","temperature"][i] = Qt
+          continue
+      except:
+        QtOLD = clusters_df["log","temperature"].values[i]
       if Qt != QtOLD:
         try:
           lf = float(clusters_df["log"]["vibrational_frequencies"].values[i][0])
