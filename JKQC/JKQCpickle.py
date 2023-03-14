1df6d5612fe49d3c4226254d794eeb5e2884c23b
Jakub Kubecka <akcebukbukaj@gmail.com>
2023-03-14 13:12:51 +0100

    JKML

--- JKQC/JKQCpickle.py
+++ JKQC/JKQCpickle.py
@@ -938,10 +938,14 @@ for file_i in files:
             out_dipole_moment = missing
           continue
         if re.search("molecular dipole:", line):
+          print("TEST 1")
+          print(line)
           save_something = "molecular_dipole"
           save_molecular_dipole = 3
           continue
         if save_something == "molecular_dipole": 
+          print("TEST 2")
+          print(line)
           save_molecular_dipole-=1
           if save_molecular_dipole == 0:
             try: 
