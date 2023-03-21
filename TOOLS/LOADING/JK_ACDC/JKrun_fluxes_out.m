572d171eb53c8187175f1ffdeca2138c34d760d0
Jakub Kubecka <kubeckaj@s96n41.grendel.cscaa.dk>
2023-03-21 18:37:06 +0100

    ACDC

--- TOOLS/LOADING/JK_ACDC/JKrun_fluxes_out.m
+++ TOOLS/LOADING/JK_ACDC/JKrun_fluxes_out.m
@@ -1,9 +1,11 @@
 clc
 clear all
-cd ../../
+if not(isfolder("RUN")) 
+  fprintf("Folder RUN missing. Have you run e.g. JKrun_single.m?")
+  exit
+end
 init
 
-cd 'RUN/'
 load("variables.mat")
 fprintf("JK: Loading ACDC output.\n");
 
@@ -20,7 +22,7 @@ end
 fclose(fp);
 get_evap; get_coll; get_wl; get_cs;
 source=zeros([size(C(end,:),2),1]);
-[flux_2d, coll_evap_2d, sources, flux_3d, flux_out, clust_flux]=dofluxes(C(end,:)*10^6,K,E,WL,CS,source,0,0,0);
+[flux_2d, coll_evap_2d, sources, flux_3d, flux_out, clust_flux]=dofluxes(C(end,:)*10^6,K,E,WL,CS,source,0);
 
 out=[];
 max=size(flux_out,1);
@@ -35,9 +37,14 @@ for i=1:max
  for j=1:max   
    f = flux_out(i,j,1);
    if f > 0.05*total %This means that you do not care about less than 5 % growth paths
-       out=[out;[clust_acdc{i}," + ",clust_acdc{j}," --> ",JKcombine_clusters(clust_acdc{i}+"",clust_acdc{j}+""),num2str(round(1000*f/total)/10)+"%"]];
+     out=[out;[clust_acdc{i},clust_acdc{j},JKcombine_clusters(clust_acdc{i}+"",clust_acdc{j}+""),f/total*100]];
    end
  end
 end
-out
-"Total flux is "+num2str(total/10^6)+" cm-3s-1"
+if size(out,1) > 1
+  out=sort(out,1,"descend");
+end
+for i = 1:size(out,1)
+  fprintf("\n%10s + %10s --> %10s (%.2f %s)\n",out(i,1),out(i,2),out(i,3),out(i,4),"%")
+end
+fprintf("\nTotal flux is %f cm-3s-1\n",total/10^6)
