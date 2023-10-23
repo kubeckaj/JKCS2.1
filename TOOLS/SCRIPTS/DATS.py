aac2c5cbc3594c7ef90a2a9cb86a6b7372846532
DanielAyoubi <danielayoubi@hotmail.com>
2023-10-23 14:34:02 +0200

    additional arguments added

--- TOOLS/SCRIPTS/DATS.py
+++ TOOLS/SCRIPTS/DATS.py
@@ -45,56 +45,62 @@ def write_xyz_file(file_path, updated_coords):
             f.write(f'{atom[0]} {atom[1]:.6f} {atom[2]:.6f} {atom[3]:.6f}\n')
 
 
-def crest_constrain(file_path, C_index, H_index, O_index, OH_index, force_constant=0.25):
+#########################################GENERATE INPUT FILES#############################
+def crest_constrain(file_path, C_index, H_index, O_index, force_constant=0.25):
     '''Force constant tells how tight to constrain the atoms i.e. the magnitude of the oscillation between the constrained atoms'''
     with open (file_path,"w") as f:
         f.write("$constrain\n")
         f.write(f"  force constant={force_constant}\n") 
         f.write(f"  distance: {C_index}, {H_index}, auto\n")
         f.write(f"  distance: {H_index}, {O_index}, auto\n")
-        f.write(f"  distance: {O_index}, {OH_index}, auto\n")
         f.write(f"  angle: {C_index}, {H_index}, {O_index}, auto\n")
         f.write("$end\n")
 
 
-def orca_constrain(file_path, coords, C_index, H_index, O_index, OH_index, method="wB97X-D3", basis_set="6-31+G(d,p)"):
+def orca_input(file_path, coords, C_index, H_index, O_index, constrain, TS, method="wB97X-D3", basis_set="6-31+G(d,p)"):
     with open(file_path, "w") as f:
-        f.write(f"! {method} {basis_set} Opt\n")
+        if TS:
+            f.write(f"! {method} {basis_set} OptTS freq\n")
+        elif TS is False:
+            f.write(f"! {method} {basis_set} Opt\n")
         f.write("%pal nprocs 8 end\n")
         f.write("%maxcore 8000\n")
-        f.write("%geom\n")
-        f.write("Constraints\n")
-        # ORCA start atom indexing from 0
-        f.write(f"{{B {C_index-1} {H_index-1} C}}\n") # Bond being broken
-        f.write(f"{{B {H_index-1} {O_index-1} C}}\n") # Bond being formed
-        f.write(f"{{B {O_index-1} {OH_index-1} C}}\n") # Bond being formed
-        f.write("end\nend\n \n * xyz 0 2\n") # charge and spin multiplicity usually 0 and 2 for atmospheric radical reaction
+        if constrain:
+            f.write("%geom\n")
+            f.write("Constraints\n")
+            # ORCA start atom indexing from 0
+            f.write(f"{{B {C_index-1} {H_index-1} C}}\n") # Bond being broken
+            f.write(f"{{B {H_index-1} {O_index-1} C}}\n") # Bond being formed
+            f.write("end\nend\n")
+        f.write("\n")
+        f.write("* xyz 0 2\n") # charge and spin multiplicity usually 0 and 2 for atmospheric radical reaction
         for atom in coords:
             f.write(f'{atom[0]} {atom[1]:.6f} {atom[2]:.6f} {atom[3]:.6f}\n')
         f.write("*")
 
 
-def gauss_constrain(file_path, coords):
-    pass
+def gauss_input(file_path, coords, C_index, H_index, O_index, constrain, TS, method="wb97xd", basis_set="6-31+G(d,p)"):
+    with open(file_path, "w") as f:
+        f.write("%nprocshared=4\n")
+        f.write("%mem=4GB\n")
+        if TS and constrain:
+            f.write(f"# {method} {basis_set} opt=(calcfc,ts,noeigen,modredundant) freq\n\n") # freq may be redundant
+        elif TS and constrain is False:
+            f.write(f"# {method} {basis_set} opt=(calcfc,ts,noeigen) freq\n\n")
+        else:
+            f.write(f"# {method} {basis_set} opt\n\n")
+        f.write("Title\n\n")
+        f.write("0 2\n")
+        for atom in coords:
+            f.write(f'{atom[0]} {atom[1]:.6f} {atom[2]:.6f} {atom[3]:.6f}\n')
+        f.write("\n")
+        if constrain:
+            f.write(f"B {C_index} {H_index} F\n")
+            f.write(f"B {H_index} {O_index} F\n")
+            f.write("\n")
 
 
-def find_atom_to_constrain(file, desired_distance=1.25):
-    coords = read_xyz_file(file)
-
-    c_index = None
-    h_index = None
-    o_index = None
-
-    for i, carbon in enumerate(coords):
-        if carbon[0] == 'O':
-            c_index = i
-            for j, hydrogen in enumerate(coords):
-                if hydrogen[0] == "H":
-                    distance_CH = atom_distance(carbon[1:], hydrogen[1:])
-
-    return c_index, h_index, o_index
-
-
+##############################ADDITIONAL FUNCTIONS##################################
 def get_terminal_O(coords, distance=1.5):
     O_index = []
     for i,atom in enumerate(coords):
@@ -107,20 +113,19 @@ def get_terminal_O(coords, distance=1.5):
 def molecule_controid(coords):
     coords = np.array(coords)
     
-    # Extract x, y, z coordinates
     x_coords = coords[:, 1].astype(float)
     y_coords = coords[:, 2].astype(float)
     z_coords = coords[:, 3].astype(float)
 
-    # Calculate centroid (average) for each coordinate
     centroid_x = np.mean(x_coords)
     centroid_y = np.mean(y_coords)
     centroid_z = np.mean(z_coords)
 
     return (centroid_x, centroid_y, centroid_z)
-######################################################################################
 
-def H_abstraction(file, distance=1.35, dist_OH=0.97, constraint=False, constrained_opt=False):
+
+#####################################MAIN FUNCTIONS####################################
+def H_abstraction(file, method, basis_set, TS, XYZ, program=None, distance=1.35, dist_OH=0.97, constrain=False):
     coords = read_xyz_file(file)
     num_atoms = len(coords)
     count = 1
@@ -163,15 +168,22 @@ def H_abstraction(file, distance=1.35, dist_OH=0.97, constraint=False, constrain
                         new_coords.append(['H', *hydrogen_coords])
 
                         base_file_name = os.path.splitext(os.path.basename(file))[0]
-                        write_xyz_file(f"{base_file_name}_H{count}.xyz", new_coords)
+                        if XYZ is True:
+                            write_xyz_file(f"{base_file_name}_H{count}.xyz", new_coords)
 
 
-                        if constrained_opt:
+                        if constrain or method is not None or basis_set is not None:
                             C_index = j+1
                             H_index = i+1
                             O_index = len(new_coords)-1
                             OH_index = len(new_coords)
-                            orca_constrain(file_path=f"{base_file_name}_H{count}.inp", coords=new_coords,C_index=C_index, H_index=H_index, O_index=O_index, OH_index=OH_index)
+                            # Default program if nothing specified is Gaussian
+                            if program == None: 
+                                gauss_input(file_path=f"{base_file_name}_H{count}.com", coords=new_coords,C_index=C_index, H_index=H_index, O_index=O_index, constrain=constrain, TS=TS, method=method, basis_set=basis_set)
+                            elif program == "orca":
+                                orca_input(file_path=f"{base_file_name}_H{count}.inp", coords=new_coords, C_index=C_index, H_index=H_index, O_index=O_index, constrain=constrain, TS=TS, method=method, basis_set=basis_set)
+                            elif program == "crest":
+                                crest_constrain(file_path=f"{base_file_name}_H{count}.inp", C_index=C_index, H_index=H_index, O_index=O_index)
 
                         count += 1
 
@@ -239,6 +251,8 @@ def addition(file1, file2, distance = 1.55, double_bond_distance=1.36, elongatio
     for index in carbon_index[::2]:
         new_coords = read_xyz_file(file1)
         direction = calculate_vector(new_coords[index][1:], new_coords[index+1][1:]) # Vector between C=C
+        # perpendicular_vector = [calculate_vector(coords1[index][1:], coords1[k][1:]) for k in range(num_atoms1) if atom_distance(coords1[index][1:], coords1[k][1:]) < 1.52]
+
         for k in range(num_atoms1):
             if atom_distance(coords1[index][1:], coords1[k][1:]) < 1.52:
                 perpendicular_vector = calculate_vector(coords1[index][1:], coords1[k][1:])
@@ -302,41 +316,91 @@ def main():
     
     parser = argparse.ArgumentParser(description='TS Generation Tool',
                                      prog="JKTS",
-                                     epilog="")
+                                     epilog='''
+
+                                     Examples of use:\n
+
+                                     JKTS file.xyz -H -orca -method R2SCAN-3C\n
+                                     JKTS reactant.log product.log -k\n 
+
+                                     ''')
     
     parser.add_argument('reactant1', metavar='file1.xyz', help='First reactant XYZ file (e.g., pinonaldehyde.xyz)')
     parser.add_argument('reactant2', nargs='?', metavar='file2.xyz', help='Second reactant XYZ file (e.g., peroxy.xyz)')
+
     parser.add_argument('-CC', action='store_true', help='Perform addition to C=C')
     parser.add_argument('-H', action='store_true', help='Perform H abstraction')
-    parser.add_argument('Program', action='store_true', help='Gaussian or ORCA')
-    parser.add_argument('-constrained_opt', action='store_true', help='Create constraint.inp file')
-    parser.add_argument('-k', action='store_true', help='Write me later')
+    parser.add_argument('-gauss', action='store_true', help='Create Gaussian16 input file [defualt]')
+    parser.add_argument('-orca', action='store_true', help='Create ORCA input file')
+    parser.add_argument('-crest', action='store_true', help='Create crest input file')
+    parser.add_argument('-constrain', action='store_true', help='Constrain is integrated into input file')
+
+    additional_options = parser.add_argument_group("Additional arguments")
+
+    additional_options.add_argument('--no-TS',action='store_true', help='Input files for normal geometry relaxation are generated')
+    additional_options.add_argument('--no-XYZ',action='store_true', help='No XYZ files generated')
+    additional_options.add_argument('-k', action='store_true', help='Write me later')
+    additional_options.add_argument('-method', dest='method_name', help='Specify the QC method [def = wB97X-D]')
+    additional_options.add_argument('-basis', dest='basis_name', help='Specify the basis set used [def = 6-31+G(d,p)]')
+
 
     args = parser.parse_args()
 
     if args.reactant1 and args.reactant2 == None:
-        file_in = args.reactant1
+        file1 = args.reactant1
     elif args.reactant1 and args.reactant2:
         file1 = args.reactant1
         file2 = args.reactant2
     else:
         parser.error('Please provide xyz file(s) of reactant(s)')
+    
+    # Program specification
+    if args.gauss:
+        program = "gauss"
+    elif args.orca:
+        program = "orca"
+    elif args.crest:
+        program = "crest"
+    else: program = None # Just produce XYZ-file
 
+    # Method specification
+    if args.method_name:
+        method = args.method_name
+    elif args.method_name not in args and program == "orca":
+        method = "WB97X-D3" 
+    else: method = "wb97xd"
 
-    if args.reactant1:
+    # Basis set specification
+    if args.basis_name:
+        basis_set = args.basis_name
+    else: basis_set = "6-31+G(d,p)"
+
+    # Constrain specification
+    if args.constrain:
+        constrain = True
+    else: constrain = False
+
+    if args.no_TS is True:
+        TS = False
+    else: TS = True
+
+    if args.no_XYZ is True:
+        XYZ = False
+    else: XYZ = True
+
+    if args.reactant1 and args.reactant2 not in args:
         if args.H:
-            if args.constrained_opt:
-                H_abstraction(file_in, constrained_opt=True)
-            else:
-                H_abstraction(file_in)
-        elif args.CC:
-            OH_addition(file_in)
-        elif args.reactant1 and args.reactant2:
-            addition(file1, file2)
-        elif args.k:
-            rate_constant_gauss(file_in)
+            H_abstraction(file1, constrain=constrain, TS=TS, program=program, method=method, basis_set=basis_set, XYZ=XYZ)
         else:
-            parser.error('Please specify either -H or -CC or another xyz-file.')
+            parser.error("Please specify either -H, -CC or another xyz-file")
+
+    elif args.reactant1 and args.reactant2:
+        if args.CC:
+            addition(file1, file2, constrain=constrain, program=program, method=method, basis_set=basis_set)
+        else:
+            parser.error("Please specify either -H, -CC or another xyz-file")
+
+            
 
 if __name__ == "__main__":
     main()
