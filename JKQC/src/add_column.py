d169d06216bad904ae48d688ec6501a03612224b
kubeckaj <akcebukbukaj@gmail.com>
2025-01-29 10:28:44 +0100

    add list as new column

--- JKQC/src/add_column.py
+++ JKQC/src/add_column.py
@@ -1,7 +1,8 @@
 def add_column(clusters_df,addcolumn):
   from numpy import dtype,array
   
-  dtype = dtype([("label", "U100"), ("value", float)])
+  dtype = dtype([("label", "U100"), ("value", object)])
+
   for i in range(len(addcolumn)):
     if addcolumn[i][0] == "SP" and addcolumn[i][1][-3:] == "pkl":
       from pandas import read_pickle
@@ -13,7 +14,10 @@ def add_column(clusters_df,addcolumn):
     else:
       from numpy import loadtxt
       loadedmatrix = loadtxt(addcolumn[i][1], usecols=(0, 1), unpack=True, dtype=dtype)
-      loadeddictionary = {loadedmatrix[0][idx] : loadedmatrix[1][idx] for idx in range(len(loadedmatrix[1]))}
+      try:
+        loadeddictionary = {loadedmatrix[0][idx] : loadedmatrix[1][idx] for idx in range(len(loadedmatrix[1]))}
+      except:
+        loadeddictionary = {loadedmatrix[0].item() : loadedmatrix[1].item()}
       tobeadded = clusters_df.loc[:,("info",'file_basename')].map(loadeddictionary)
       clusters_df.loc[tobeadded.index,("extra",addcolumn[i][0])] = tobeadded.values
  
