4549b6089b6c4a36e19bbd94ee4b6ad300cfafac
Lauri Seppäläinen <lauri.seppalainen@helsinki.fi>
2025-03-24 15:54:02 +0100

    add kernel-based distance function

--- JKML/src/QKNN.py
+++ JKML/src/QKNN.py
@@ -8,7 +8,7 @@ import pickle
 from sklearn.neighbors import KNeighborsRegressor
 from metric_learn import MLKR
 from ase.atoms import Atoms
-from typing import List, Tuple, Union, Dict
+from typing import Iterable, Tuple, Union, Dict
 import os
 from collections import defaultdict
 import time
@@ -21,7 +21,7 @@ warnings.filterwarnings(
 )
 
 
-def _generate_fchl19(strs: List[Atoms], rcut=8.0, acut=8.0, **kwargs) -> np.ndarray:
+def _generate_fchl19(strs: Iterable[Atoms], rcut=8.0, acut=8.0, **kwargs) -> np.ndarray:
     from qmllib.representations import generate_fchl19 as generate_representation
 
     n = len(strs)
@@ -45,7 +45,7 @@ def _generate_fchl19(strs: List[Atoms], rcut=8.0, acut=8.0, **kwargs) -> np.ndar
     return X
 
 
-def _generate_mbdf(strs: List[Atoms], cutoff: float = 8.0, **kwargs) -> np.ndarray:
+def _generate_mbdf(strs: Iterable[Atoms], cutoff: float = 8.0, **kwargs) -> np.ndarray:
     from MBDF import generate_mbdf as generate_representation
 
     n = len(strs)
@@ -64,7 +64,7 @@ def _generate_mbdf(strs: List[Atoms], cutoff: float = 8.0, **kwargs) -> np.ndarr
 
 
 def _generate_bob(
-    strs: List[Atoms],
+    strs: Iterable[Atoms],
     max_atoms: int = None,
     asize: Dict[str, Union[np.int64, int]] = None,
     **kwargs,
@@ -107,7 +107,7 @@ def _generate_bob(
     return X
 
 
-def _generate_coulomb(strs: List[Atoms], max_atoms: int = None, **kwargs):
+def _generate_coulomb(strs: Iterable[Atoms], max_atoms: int = None, **kwargs):
     from qmllib.representations import (
         generate_coulomb_matrix as generate_representation,
     )
@@ -128,7 +128,7 @@ def _generate_coulomb(strs: List[Atoms], max_atoms: int = None, **kwargs):
 
 
 def _generate_mbtr(
-    strs: List[Atoms],
+    strs: Iterable[Atoms],
     k2_width=0.1,
     k2_weight=0.5,
     k3_width=0.1,
@@ -163,6 +163,24 @@ def _generate_mbtr(
     return X
 
 
+def _generate_fchl18(strs: Iterable[Atoms], cut_distance=8.0):
+    from qmllib.representations import generate_fchl18 as generate_representation
+
+    max_atoms = max([len(s.get_atomic_numbers()) for s in strs])
+    representations = []
+    for struct in strs:
+        representations.append(
+            generate_representation(
+                struct.get_atomic_numbers(),
+                struct.get_positions(),
+                max_size=max_atoms,
+                neighbors=max_atoms,
+                cut_distance=cut_distance,
+            )
+        )
+    return np.array(representations)
+
+
 def calculate_representation(Qrepresentation, strs, **repr_kwargs):
     if Qrepresentation == "fchl":
         return _generate_fchl19(strs, **repr_kwargs)
@@ -174,10 +192,76 @@ def calculate_representation(Qrepresentation, strs, **repr_kwargs):
         return _generate_coulomb(strs, **repr_kwargs)
     elif Qrepresentation == "mbtr":
         return _generate_mbtr(strs, **repr_kwargs)
+    elif Qrepresentation == "fchl-kernel":
+        return _generate_fchl18(strs, **repr_kwargs)
     else:
         raise NotImplementedError(
             f"Representation 'f{Qrepresentation}' not supported with the k-NN model!"
         )
+
+
+def correct_fchl18_kernel_size(X_test: np.ndarray, X_train: np.ndarray):
+    if X_train.shape[1] != X_test.shape[1]:
+        if X_train.shape[1] > X_test.shape[1]:
+            small = X_test
+            large = X_train
+        else:
+            small = X_train
+            large = X_test
+        newmatrix = np.zeros([small.shape[0], large.shape[1], 5, large.shape[3]])
+        newmatrix[:, :, 0, :] = 1e100
+        newmatrix[0 : small.shape[0], 0 : small.shape[1], 0:5, 0 : small.shape[3]] = (
+            small
+        )
+        if X_train.shape[1] > X_test.shape[1]:
+            X_test = newmatrix
+        else:
+            X_train = newmatrix
+    return X_test, X_train
+
+
+def calculate_kernel(X: np.ndarray, X_other: np.ndarray = None, **kernel_kwargs):
+    from qmllib.representations.fchl import (
+        get_local_symmetric_kernels as JKML_sym_kernel,
+    )
+    from qmllib.representations.fchl import get_local_kernels as JKML_kernel
+
+    if X_other is None:
+        return JKML_kernel(X, X_other, **kernel_kwargs)
+    else:
+        return JKML_sym_kernel(X, **kernel_kwargs)
+
+
+def induced_kernel_distance(
+    K: np.ndarray, K_train: np.ndarray = None, K_test: np.ndarray = None
+):
+
+    def _remove_extra_dim(K: np.ndarray, kernel_name: str):
+        # FCHL kernels return a matrix with an extra leading dimension; this function drops that.
+        if K.ndim > 2:
+            if K.shape[0] == 1:
+                K = K[0]
+            else:
+                raise ValueError(
+                    f"Incompatible {kernel_name} kernel shape! (got shape {K.shape})"
+                )
+        return K
+
+    K = _remove_extra_dim(K, "K")
+    if K_train is None:
+        diag_1 = diag_2 = np.diag(K)
+    else:
+        assert K_test is not None, "Need both self-similarity kernels!"
+        K_train = _remove_extra_dim(K_train, "train")
+        K_test = _remove_extra_dim(K_test, "test")
+        diag_1 = np.diag(K_test)
+        diag_2 = np.diag(K_train)
+    # first two terms broadcast the self-similarities to a [n x m] matrix
+    D_squared = diag_1[:, None] + diag_2[None, :] - 2 * K
+    # get rid of possible numerical problems
+    D_squared = np.maximum(D_squared, 0.0)
+    D = np.sqrt(D_squared)
+    return D
 
 
 def load_hyperparams(hyper_cache: str):
@@ -208,6 +292,7 @@ def training(
     hyperparams = load_hyperparams(hyper_cache)
     # Convert structures to list to avoid indexing issues
     strs = strs.values
+
     ### REPRESENTATION CALCULATION ###
     repr_wall_start = time.perf_counter()
     repr_cpu_start = time.process_time()
@@ -219,6 +304,10 @@ def training(
     X_train = calculate_representation(
         Qrepresentation, strs, **hyperparams["representation"]
     )
+    if Qrepresentation == "fchl-kernel":
+        print("JKML(k-NN): Calculate train kernels.", flush=True)
+        K_train = calculate_kernel(X_train, **hyperparams["kernel"])
+        D_train = induced_kernel_distance(K_train)
     repr_train_wall = time.perf_counter() - repr_wall_start
     repr_train_cpu = time.process_time() - repr_cpu_start
 
@@ -246,9 +335,14 @@ def training(
             algorithm="ball_tree",
             **hyperparams["knn"],
         )
+        knn.fit(X_train, Y_train)
+    elif Qrepresentation == "fchl-kernel":
+        knn = KNeighborsRegressor(metric="precomputed", n_jobs=-1, **hyperparams["knn"])
+        knn.fit(D_train, Y_train)
     else:
+        # "vanilla" k-NN
         knn = KNeighborsRegressor(n_jobs=-1, algorithm="auto", **hyperparams["knn"])
-    knn.fit(X_train, Y_train)
+        knn.fit(X_train, Y_train)
     train_wall = time.perf_counter() - train_wall_start
     train_cpu = time.process_time() - train_cpu_start
     n_train, d_train = X_train.shape
@@ -261,17 +355,41 @@ def training(
         "d_train": d_train,
     }
     print("JKML(Q-kNN): Training completed.", flush=True)
+    if Qrepresentation != "fchl-kernel":
+        K_train = None
     knn_params = knn.get_params()
     if not no_metric:
         knn_params["metric"] = "MLKR_placeholder"
     with open(varsoutfile, "wb") as f:
         print(f"JKML(Q-kNN): Saving training data to {varsoutfile}")
-        if not no_metric:
+        if no_metric:
+            pickle.dump([X_train, Y_train, X_atoms, knn_params, train_metadata], f)
+        elif Qrepresentation == "fchl-kernel":
             pickle.dump(
-                [X_train, Y_train, X_atoms, A, mlkr, knn_params, train_metadata], f
+                [
+                    X_train,
+                    Y_train,
+                    X_atoms,
+                    K_train,
+                    D_train,
+                    knn_params,
+                    train_metadata,
+                ],
+                f,
             )
         else:
-            pickle.dump([X_train, Y_train, X_atoms, knn_params, train_metadata], f)
+            pickle.dump(
+                [
+                    X_train,
+                    Y_train,
+                    X_atoms,
+                    A,
+                    mlkr,
+                    knn_params,
+                    train_metadata,
+                ],
+                f,
+            )
     return {
         key: value
         for key, value in locals().items()
@@ -279,6 +397,7 @@ def training(
         in [
             "X_train",
             "X_atoms",
+            "K_train",
             "A",
             "knn",
             "repr_train_wall",
@@ -296,7 +415,7 @@ def training(
 ###############################################################################
 
 
-def evaluate(Qrepresentation, X_train, strs, knn_model, hyper_cache=None):
+def evaluate(Qrepresentation, X_train, strs, knn_model, K_train=None, hyper_cache=None):
 
     import numpy as np
 
@@ -308,40 +427,27 @@ def evaluate(Qrepresentation, X_train, strs, knn_model, hyper_cache=None):
     X_test = calculate_representation(
         Qrepresentation, strs, **hyperparams["representation"]
     )
+    if Qrepresentation == "fchl-kernel":
+        X_test, X_train = correct_fchl18_kernel_size(X_test, X_train)
+        print("JKML(k-NN): Calculate test kernel(s).", flush=True)
+        K_test = calculate_kernel(X_test)
+        K = calculate_kernel(X_test, X_train)
+        D_test = induced_kernel_distance(K, K_train, K_test)
     repr_test_wall = time.perf_counter() - repr_wall_start
     repr_test_cpu = time.process_time() - repr_cpu_start
 
     # some info about the full representation
     print(
-        "JKML(QML): Shape of the testing representation: " + str(X_test.shape),
+        "JKML(k-NN): Shape of the testing representation: " + str(X_test.shape),
         flush=True,
     )
 
-    ### CORRECTING THE FCHL MATRIX SIZES
-    # IF YOU ARE EXTENDING THIS WILL MAKE THE MATRIXES OF THE SAME SIZE
-    # THIS IS COMMENTED OUT AS IT DOES NOT APPLY TO FCHL'19 (global)
-    #    if Qrepresentation == "fchl":
-    #        if X_train.shape[1] != X_test.shape[1]:
-    #            if X_train.shape[1] > X_test.shape[1]:
-    #                small = X_test
-    #                large = X_train
-    #            else:
-    #                small = X_train
-    #                large = X_test
-    #            newmatrix = np.zeros([small.shape[0], large.shape[1], 5, large.shape[3]])
-    #            newmatrix[:, :, 0, :] = 1e100
-    #            newmatrix[
-    #                0 : small.shape[0], 0 : small.shape[1], 0:5, 0 : small.shape[3]
-    #            ] = small
-    #            if X_train.shape[1] > X_test.shape[1]:
-    #                X_test = newmatrix
-    #            else:
-    #                X_train = newmatrix
-    #
-    ### THE EVALUATION
     test_wall_start = time.perf_counter()
     test_cpu_start = time.process_time()
-    Y_predicted = knn_model.predict(X_test)
+    if Qrepresentation == "fchl-kernel":
+        Y_predicted = knn_model.predict(D_test)
+    else:
+        Y_predicted = knn_model.predict(X_test)
     test_wall = time.perf_counter() - test_wall_start
     test_cpu = time.process_time() - test_cpu_start
     Y_predicted = Y_predicted[None, :]
@@ -396,6 +502,9 @@ def hyperopt(
         )
         global X
         X = calculate_representation(Qrepresentation, strs)
+        if Qrepresentation == "fchl-kernel":
+            K = calculate_kernel(X)
+            X = induced_kernel_distance(K)
 
     # add k-nn specific hyperparameters
     space.append(skopt.space.Integer(3, 15, name="n_neighbors"))
