7ab65e4a3813f4ea1bae2bce7df8ca1abdddbebd
Lauri Seppäläinen <lauriseppalainen@gmail.com>
2025-04-11 10:02:52 +0200

    add time information to hyperopt

--- JKML/src/QKNN.py
+++ JKML/src/QKNN.py
@@ -917,6 +917,7 @@ def hyperopt(
             mlkrs = []
 
         for i, (train_index, test_index) in enumerate(kf.split(X)):
+            fold_start = time.perf_counter()
             X_fold, Y_fold = X[train_index], Y_train[train_index]
             X_test = X[test_index]
             if no_metric:
@@ -945,6 +946,8 @@ def hyperopt(
                 # Y_fold is still a vector; can index directly
                 Y_sorted = Y_fold[sorted_indices]
             sorted_Ys.append(Y_sorted)
+            print(f"\tFold {i+1}/{cv_folds} done, took {time.perf_counter() - fold_start:.1f} s.", flush=True)
+
 
         print(f"JKML(k-NN): Precalculation done, took {time.perf_counter() - precalc_start:.1f} s.", flush=True)
         @skopt.utils.use_named_args(space)
