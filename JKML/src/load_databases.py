df579b2d366c65b4d7993896a79d345626df0e47
kubeckaj <akcebukbukaj@gmail.com>
2025-07-04 18:43:31 +0200

    JKML database monomer check

--- JKML/src/load_databases.py
+++ JKML/src/load_databases.py
@@ -19,38 +19,38 @@ def load_databases(
 
     if Qtrain == 1 or Qhyper:
         if not os.path.exists(TRAIN_HIGH):
-            print("Error reading file. TRAIN_HIGH = " + TRAIN_HIGH)
+            print("JKML(load_databases): Error reading file. TRAIN_HIGH = " + TRAIN_HIGH)
             exit()
         if method == "delta":
             if not os.path.exists(TRAIN_LOW):
-                print("Error reading file. TRAIN_LOW = " + TRAIN_LOW)
+                print("JKML(load_databases): Error reading file. TRAIN_LOW = " + TRAIN_LOW)
                 exit()
     if Qtrain == 2:
         if not os.path.exists(VARS_PKL):
-            print("Error reading file. VARS_PKL = " + VARS_PKL)
+            print("JKML(load_databases): Error reading file. VARS_PKL = " + VARS_PKL)
             exit()
     if Qeval == 1 or Qeval == 2:
         if not os.path.exists(TEST_HIGH):
-            print("Error reading file. + TEST_HIGH = " + TEST_HIGH)
+            print("JKML(load_databases): Error reading file. + TEST_HIGH = " + TEST_HIGH)
             exit()
         if method == "delta":
             if not os.path.exists(TEST_LOW):
                 if Qeval == 1:
                     TEST_LOW = TEST_HIGH
                 else:
-                    print("Error reading file. + TEST_LOW = " + TEST_LOW)
+                    print("JKML(load_databases): Error reading file. + TEST_LOW = " + TEST_LOW)
                     exit()
     if Qmonomers == 1:
         if not os.path.exists(MONOMERS_HIGH):
-            print("Error reading file. MONOMERS_HIGH = " + MONOMERS_HIGH)
+            print("JKML(load_databases): Error reading file. MONOMERS_HIGH = " + MONOMERS_HIGH)
             exit()
         if method == "delta":
             if not os.path.exists(MONOMERS_LOW):
-                print("Error reading file. MONOMERS_LOW = " + MONOMERS_LOW)
+                print("JKML(load_databases): Error reading file. MONOMERS_LOW = " + MONOMERS_LOW)
                 exit()
 
     # IMPORTING THE REST OF LIBRARIES
-    print("JKML is loading database(s).", flush=True)
+    print("JKML(load_databases) is loading database(s).", flush=True)
 
     # LOADING THE DATABASES
     train_high_database = "none"
@@ -61,28 +61,46 @@ def load_databases(
     monomers_low_database = "none"
     if Qtrain == 1 or Qhyper:
         train_high_database = read_pickle(TRAIN_HIGH).sort_values(
-            [("info", "file_basename")]
+          [("info", "file_basename")]
         )
+        if Qmonomers == 1:
+          if not train_high_database.loc[:,("info", "components")].notna().all():
+            print("JKML(load_databases): train_high_database are missing some info:components.")
+            exit()
         if method == "delta":
             train_low_database = read_pickle(TRAIN_LOW).sort_values(
                 [("info", "file_basename")]
             )
+            if Qmonomers == 1:
+              if not train_low_database.loc[:,("info", "components")].notna().all():
+                print("JKML(load_databases): train_low_database are missing some info:components.")
+                exit()
     if Qeval == 1 or Qeval == 2 or Qopt > 0:
         test_high_database = read_pickle(TEST_HIGH).sort_values(
             [("info", "file_basename")]
         )
+        if Qmonomers == 1:
+          if not test_high_database.loc[:,("info", "components")].notna().all():
+            print("JKML(load_databases): test_high_database are missing some info:components.")
+            exit()
         if method == "delta":
             test_low_database = read_pickle(TEST_LOW).sort_values(
                 [("info", "file_basename")]
             )
+            if Qmonomers == 1:
+              if not test_low_database.loc[:,("info", "components")].notna().all():
+                print("JKML(load_databases): test_low_database are missing some info:components.")
+                exit()
     if Qmonomers == 1:
-        monomers_high_database = read_pickle(MONOMERS_HIGH).sort_values(
-            [("info", "file_basename")]
-        )
+        monomers_high_database = read_pickle(MONOMERS_HIGH).sort_values([("info", "file_basename")])
+        if not monomers_high_database.loc[:,("info", "components")].notna().all():
+          print("JKML(load_databases): Monomers are missing some info:components.")
+          exit()
         if method == "delta":
-            monomers_low_database = read_pickle(MONOMERS_LOW).sort_values(
-                [("info", "file_basename")]
-            )
+          monomers_low_database = read_pickle(MONOMERS_LOW).sort_values([("info", "file_basename")])
+          if not monomers_low_database.loc[:,("info", "components")].notna().all():
+            print("JKML(load_databases): Monomers are missing some info:components.")
+            exit()
 
     return (
         train_high_database,
